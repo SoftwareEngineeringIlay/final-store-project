fbea43e5c5eec832ad5c4b787866c406cf2d25ba
Ilay_Nachshon <ilay.na958@gmail.com>
2025-04-13 13:15:45 +0300

    Improved styling.

--- src/components/todo/todo.component.ts
+++ src/components/todo/todo.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
   tasks: string[] = [];
+
+  ngOnInit() {
+    this.loadTasks();
+  }
+
+  loadTasks() {
+    fetch('http://localhost:3000/tasks')
+      .then(res => res.json())
+      .then((data: { messages: string[] }) => {
+        this.tasks = data.messages;
+      });
+  }
 
   processTaskInput(value: string) {
     const task = value.trim();
     if (!task) return;
-
-    fetch(`http://localhost:3000/add/${encodeURIComponent(task)}`)
+    
+    fetch('http://localhost:3000/add/' + encodeURIComponent(task))
       .then(res => res.json())
       .then((data: { messages: string[] }) => {
         this.tasks = data.messages;
