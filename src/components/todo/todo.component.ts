6458cbdaac000779926c8c15d0b949dffb216b78
Ilay_Nachshon <ilay.na958@gmail.com>
2025-04-13 22:10:08 +0300

    Success to style the tasks.

--- src/components/todo/todo.component.ts
+++ src/components/todo/todo.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
-  templateUrl: './todo.component.html'
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
 })
 export class TodoComponent {
   newTitle: string = '';
